dc49b50145a961f70f76ef3bdac306a5e2a03af0
mbukeRepo <mbuke404@gmail.com>
2023-12-16 14:02:16 +0200

    fix: add more functions to the menu mod

--- fundamentals/src/bin/mini_bill.rs
+++ fundamentals/src/bin/mini_bill.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::collections::HashMap;
 
 fn get_input() -> Option<String> {
 	let mut buffer = String::new();
@@ -14,16 +15,112 @@ fn get_input() -> Option<String> {
 	}
 }
 
+fn get_bill_amount() -> Option<f64> {
+	println!("Amount: ");
+	loop {
+		let input = match get_input() {
+			Some(input) => input,
+			None => return None
+		};
+
+		if &input == "" {
+			return None;
+		}
+
+		let parsed_input: Result<f64, _> = input.parse();
+		match parsed_input {
+			Ok(amount) => return Some(amount),
+			Err(_) => println!("Please enter a valid amount")
+		}
+	}
+}
+
 enum MainMenu {
 	AddBill,
-	ViewBill
+	ViewBill,
+	RemoveBill
+}
+
+#[derive(Debug, Clone)]
+pub struct Bill {
+	name: String,
+	amount: f64,
+}
+
+pub struct Bills {
+	inner: HashMap<String, Bill>
+}
+
+impl Bills {
+	fn new() -> Self {
+		Self {
+			inner: HashMap::new()
+		}
+	}
+
+	fn add(&mut self, bill: Bill) {
+		self.inner.insert(bill.name.to_string(), bill);
+	}
+
+	fn get_all(&self) -> Vec<&Bill> {
+		self.inner.values().collect()
+	}
+
+	fn remove(&mut self, name: &str) -> bool {
+		self.inner.remove(name).is_some()
+	}
+}
+
+mod menu {
+	use crate::{Bills, Bill, get_input, get_bill_amount};
+	pub fn add_bill(bills: &mut Bills) {
+		println!("Bill name: ");
+		let name = match get_input() {
+			Some(input) => input,
+			None => return
+		};
+
+		let amount = match get_bill_amount() {
+			Some(input) => input,
+			None => return
+		};
+
+		let bill = Bill {name, amount};
+		bills.add(bill);
+		println!("Bill added...");
+	}
+
+	pub fn remove_bill(bills: &mut Bills) {
+		for bill in bills.get_all() {
+			println!("{:?}", bill);
+		}
+
+		println!("Enter bill name to remove: ");
+		let name = match get_input() {
+			Some(name) => name,
+			None => return,
+		};
+
+		if bills.remove(&name) {
+			println!("bill removed");
+		} else {
+			println!("bill not found");
+		}
+	}
+
+	pub fn view_bill(bills: &Bills) {
+		for bill in bills.get_all() {
+			println!("{:?}", bill);
+		}
+	}
 }
 
 impl MainMenu {
 	fn from_str(input: &str) -> Option<MainMenu>{
 		match input {
-			"1" => Some(MainMenu::AddBill),
+			"1" => Some(Self::AddBill),
 			"2" => Some(Self::ViewBill),
+			"3" => Some(Self::RemoveBill),
 			_ => None
 		}
 	}
@@ -33,19 +130,23 @@ impl MainMenu {
 		println!("== Bill Manager ==");
 		println!("1. Add Bill");
 		println!("2. View Bill");
+		println!("3. Remove Bill");
 		println!("");
 		println!("Enter selection: ");
 	}
 }
 
 fn main() {
+	let mut bills = Bills::new();
 	loop {
 		MainMenu::show();
 		let input = get_input().expect("no data entered");
+		
 
 		match MainMenu::from_str(input.as_str()) {
-			Some(MainMenu::AddBill) => (),
-			Some(MainMenu::ViewBill) => (),
+			Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
+			Some(MainMenu::ViewBill) => menu::view_bill(&bills),
+			Some(MainMenu::RemoveBill) => menu::remove_bill(&mut bills),
 			None => return
 		}
 		
